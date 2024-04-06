edaab53b36f65af0d6eb75e4c42d1b8c202e946e
MekaDeni <deni.den.96@mail.ru>
2024-04-06 15:55:58 +0300

    i'm creat mains vareable and print their

---
+++ calculator/main.py
@@ -0,0 +1,7 @@
+a = float(input("first num > "))
+b = float(input("second num > "))
+c = input("what will we do > ")
+result = c
+
+print("Task > " + str(int(a)) + " " + str(c) + " " + str(int(b)))
+print("This is result = " + str(result))
